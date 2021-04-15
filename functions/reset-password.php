2bb10cd0f25a21a22fa5ceb8b5888db49278770b
calebbala <calebbala15@gmail.com>
2021-04-15 10:18:43 +0000

    finished with the password recovery

--- functions/reset-password.php
+++ functions/reset-password.php
@@ -1,13 +1,50 @@
 <?php
-if (isset($_POST['password'])) {
+session_start();
+if (isset($_POST['new-password'])) {
     /***
      * validate user new passwords
      */
+    $error = [];
+
+    if (empty($_POST['new-password'])) {
+        $error[] = "Password field is empty";
+    }
+    if (empty($_POST['confirm-password'])) {
+        $error[] = "Password confirmation field is empty";
+    }
+    if ($_POST['confirm-password'] != $_POST['new-password']  ) {
+        $error[] = " Password confirmation do not match";
+      }
+    if (!filter_var($_POST['email'], FILTER_VALIDATE_EMAIL)){
+       $error[] = "The email is not valid email";
+    }
+       /**
+     * if there are validation errors 
+     * set the errors in a session redirect back with error
+     * 
+     */
+    if(!empty($error)){
+        $_SESSION['errors'] = $error;
+        header('location: ../reset-password.php?email=' . $_POST['email'] .'');
+    }else{
+
+    }
+
     
-    //get the data from the database
+   //get the data from the database
     $email = stripslashes($_POST['email']);
     $path = '../data/data.json';
     $data = file_get_contents($path);
     $raw_arr = json_decode($data, true);
-    $user_data = $raw_arr[$email];
+  
+    $password = sha1(trim($_POST['new-password']));
+
+    //set the new password into the database
+    $raw_arr[$email]['password'] = $password;
+  
+   $new_data =  json_encode($raw_arr);
+   file_put_contents($path, $new_data);
+
+   //Redirect the user to a congratulations page
+   header('location:../password-changed.php');
 }
--- reset-password.php
+++ reset-password.php
@@ -24,11 +24,12 @@ if (isset($_SESSION['email'])) {
         unset($_SESSION['errors']);
     }
 ?>
-   <form  action="functions/forgot-pass-func.php" method="POST">
+   <form  action="functions/reset-password.php" method="POST">
       <div class="email-login">
-         <input type="password" placeholder="New password" name="new-password" required>
+         <input type="password" placeholder="New password" name="new-password" required autofocus>
          <label for="confirm-password"> <b>Confirm password</b></label>
-         <input type="password" placeholder="Confirm password" name="confirm-password" required>
+         <input type="password" placeholder="Confirm password" name="confirm-password" required >
+         <input type="hidden" name="email" value="<?=$_GET['email']?>">
       </div>
       <button class="cta-btn">Reset password</button>
      <span class="go-back forget-pass" onclick="window.history.back()">Go back</span>
